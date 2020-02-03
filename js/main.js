de2cfd1195e2bb2a4fae19f66e338ca42828f985
Brianna Fisher <Bnfisher4@gmail.com>
2020-02-03 10:57:52 -0600

    adds latest update

--- js/main.js
+++ js/main.js
@@ -1,35 +1,37 @@
 /*----- constants -----*/
-const colors = {
-    null: 'empty',
-    const players = {
-        '1': 'blue',
-        '-1': 'green',
-    }
+const PLAYERS = {
+        '1': 'X',
+        '-1': 'O',
+        null: ''
 };
 
-const winnerCombo = {
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+const WINNERCOMBO = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 6, 9],
-    [7, 5, 3],
-    [1, 5, 9]
-};
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 /*----- app's state (variables) -----*/
 let board = [1, 2, 3, 4, 5 ,6 ,7 ,8 ,9];
-let turn = players * -1;
-let winner = winnerCombo;
+let turn = PLAYERS * -1;
+let winner = WINNERCOMBO;
 
 
 /*----- cached element references -----*/
-const cells = document.querySelectorAll('cell');
-
+const cellsEls = document.querySelectorAll('.cell');
+const messageEl = getElementById('message');
 
 
 /*----- event listeners -----*/
+document.getElementById('game-board').addEventListener('click', handleClick);
 
 
 /*----- functions -----*/
+function handleClick(evt) {
+    console.log('evt.target.dataset.index');
+}
