763684c35eb896fe0967d3c8695c1e84f72ba64d
Brianna Fisher <Bnfisher4@gmail.com>
2020-02-04 16:10:57 -0600

    add styling and complete function

--- js/main.js
+++ js/main.js
@@ -17,21 +17,90 @@ const WINNERCOMBO = [
 ];
 
 /*----- app's state (variables) -----*/
-let board = [1, 2, 3, 4, 5 ,6 ,7 ,8 ,9];
-let turn = PLAYERS * -1;
-let winner = WINNERCOMBO;
+let board;
+let turn;
+let winner;
 
 
 /*----- cached element references -----*/
-const cellsEls = document.querySelectorAll('.cell');
-const messageEl = getElementById('message');
+const cellEls = document.querySelectorAll('.cell');
+const messageEl = document.getElementById('message');
+const playmessageEl = document.getElementById('playmessage');
 
 
 /*----- event listeners -----*/
 document.getElementById('game-board').addEventListener('click', handleClick);
+document.querySelector('button').addEventListener('click', init);
 
 
 /*----- functions -----*/
 function handleClick(evt) {
-    console.log('evt.target.dataset.index');
+    if(playmessageEl.textContent != "") {
+        playmessageEl.textContent = ""
+    }
+    let selectedIndex = evt.target.dataset.index;
+    if(winner || board[selectedIndex]) return;
+
+    board[selectedIndex] = turn; // 1
+    // toggle the turn
+    turn *= -1;
+    // check for winner
+    winner = checkWinner();
+    // visualize what just happened -> render();
+    render();
 }
+
+
+init();
+
+function init() {
+    playmessageEl.textContent = "Let's Play";
+    // We need to decide who starts the game
+    turn = 1;
+    board = [null, null, null, null, null, null, null, null, null];
+    winner = false;
+    // We need to reset the game board
+    // We also need to remove the winner
+    // We then need to visualize what a new game should look like
+    render();
+}
+
+
+// we write our logic for determining the winner
+// return true if winner or false if no winner
+function checkWinner() {
+    for (let i = 0; i < WINNERCOMBO.length; i++) {
+        if(Math.abs(board[WINNERCOMBO[i][0]] + 
+                    board[WINNERCOMBO[i][1]] + 
+                    board[WINNERCOMBO[i][2]]) === 3) return board[WINNERCOMBO[i][0]];
+    }
+    if(board.includes(null)) return false;
+    return 'T';
+}
+
+
+function render() {
+    // Draws the game board
+    board.forEach(function(element, index) {
+        cellEls[index].textContent = PLAYERS[element]
+    });
+    if(!winner) {
+        messageEl.textContent = `${PLAYERS[turn]}'s Turn`
+    } else if (winner === 'T') {
+        messageEl.textContent = "CAT!"
+    } else if (winner) {
+        messageEl.textContent = `${PLAYERS[winner]} WINS!`
+    };
+
+    
+    // What data do we use to draw the game board?
+    // Render transfers the state of the app to the DOM
+
+    console.log('Gameboard: ', board)
+}
+
+/* if (board === null) {
+    playmessageEl.textContent = "Let's Play!";
+} else {
+    playmessageEl.textContent = '';
+} */
